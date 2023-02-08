4c5233a1cba7af13c070a1cfcb9db46f508bb9e4
VIGNESH-TRL769 <123439677+VIGNESH-TRL769@users.noreply.github.com>
2023-02-08 19:38:27 +0530

    Add files via upload

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,97 @@
+package org.example;
+import java.util.Scanner;
+import java.util.logging.Logger;
+interface Icalculator
+{
+     void calc(int value);
+}
+class Operation
+{
+    public static final Logger Log=Logger.getLogger(("InfoLogging"));
+    
+    public static int result;
+    private int value;
+
+    public void setValue(int value)
+    {
+        this.value=value;
+    }
+    public int getValue()
+    {
+        return value;
+    }
+
+   
+    public void display()
+    {
+        String print="Result:"+result; 
+        Log.info(print);
+    }
+}
+class Addition extends Operation implements Icalculator
+{
+    public void calc(int value)
+    {
+        result=result+value;
+    }
+}
+class Subtraction extends Operation implements Icalculator
+{
+    public void calc(int value)
+    {
+        result=result-value;
+    }
+}
+class Division extends Operation implements Icalculator
+{
+    public void calc(int value)
+    {
+        result=result/value;
+    }
+}
+class Multiplication extends Operation implements Icalculator
+{
+    public void calc(int value)
+    {
+        result=result*value;
+    }
+}
+public class Calculator
+{
+    public static final Logger Log=Logger.getLogger(("InfoLogging"));
+    public static void main(String[] args)
+    {
+        Addition add=new Addition();
+        Subtraction sub=new Subtraction();
+        Division div=new Division();
+        Multiplication mul=new Multiplication();
+        Scanner input=new Scanner(System.in);
+        Log.info("Enter the Value:");
+        Operation.result = input.nextInt();
+        int value=0;
+        while(true)
+        {
+            Log.info("1.Addtion(+)\n2.Substration(-)\n3.Multiplication(*)\n4.Division(/)\n5.Result\n6.Exit\nEnter The Choice:\n");
+            int key = input.nextInt();
+            if(key!=5)
+            {
+                Log.info("Enter the Value:");
+                int val = input.nextInt();
+                add.setValue(val);
+                value = add.getValue();
+            }
+            switch (key) {
+                case 1 -> add.calc(value);
+                case 2 -> sub.calc(value);
+                case 3 -> mul.calc(value);
+                case 4 -> div.calc(value);
+                case 5 -> add.display();
+                case 6 ->
+                {
+                    Log.info("-----Thannk You-----");
+                    return;
+                }
+            }
+        }
+    }
+}
